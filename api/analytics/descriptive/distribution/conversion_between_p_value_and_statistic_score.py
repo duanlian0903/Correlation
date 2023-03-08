1407691ff08d00cb76be7737511eb6a6e78e6ef4
lian <duanlian.cn@gmail.com>
2023-03-08 14:52:59 -0500

    refactor

--- api/analytics/descriptive/distribution/conversion_between_p_value_and_statistic_score.py
+++ api/analytics/descriptive/distribution/conversion_between_p_value_and_statistic_score.py
@@ -1,17 +1,17 @@
 import scipy.stats as ss
 
 
-def get_chi_square_p_value(chi_square_score, degree_of_freedom):
+def get_p_value_from_chi_square(chi_square_score, degree_of_freedom):
     return 1 - ss.chi2.cdf(chi_square_score, degree_of_freedom)
 
 
-def get_chi_square_score(p_value, degree_of_freedom):
+def get_chi_square_score_from_p_value(p_value, degree_of_freedom):
     return ss.chi2.ppf(1 - p_value, degree_of_freedom)
 
 
-def get_normal_distribution_p_value(z_score):
+def get_p_value_from_z_score(z_score):
     return 1 - ss.norm.cdf(z_score)
 
 
-def get_normal_distribution_z_score(p_value):
+def get_z_score_from_p_value(p_value):
     return ss.norm.ppf(1 - p_value)
